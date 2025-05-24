837f7e5138ae1fb9ff4a515ae81981259415ecdf
Surinder <Surinder2010@outlook.com>
2025-05-24 17:10:25 -0400

    added new query to delete item from sitecore and added documentation

--- DemoSitecoreCloud/QueryBuilder.cs
+++ DemoSitecoreCloud/QueryBuilder.cs
@@ -12,51 +12,18 @@ using GraphQL.Client.Serializer.Newtonsoft;
 
 namespace DemoSitecoreCloud
 {
+    /// <summary>
+    /// Builds graph ql queries for sitecore operations
+    /// </summary>
     public class QueryBuilder
     {
         public QueryBuilder() { }
 
-        public static GraphQLRequest CreateGraphQLQuery(string itemName, Dictionary<string, string> nodes, string templateId, string parentId)
-        {
-
-            var fieldsBuilder = new StringBuilder();
-            foreach (var node in nodes)
-            {
-                fieldsBuilder.Append($"{{name: \"{ node.Key}\", value: \"{node.Value}\" }}" );
-            }
-
-            var query = $@"
-                        mutation {{
-                            createItem(
-                                     input: {{ 
-                                             name: ""{itemName}""
-                                             templateId: ""{templateId}""
-                                             parent: ""{parentId}""
-                                             language: ""en""
-                                              fields[
-                                                 {fieldsBuilder.ToString().Trim()}
-                                            ]
-                                         }}
-                                     ){{
-                                        item {{
-                                            itemId
-                                            name
-                                            path    
-                                            fields(ownFields:true, excludeStandardfields: true){{
-                                                  nodes {{ 
-                                                           name 
-                                                           value
-                                                       }}
-                                                 }}
-                                            }}
-                                        }}
-                                 }}";
-            return new GraphQLRequest
-            {
-                Query = query,
-            };
-        }
-
+        /// <summary>
+        /// Query to retrieve item from sitecore based on Item Id
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
         public static GraphQLRequest GetItemGraphQLQuery(string itemId)
         {
             var query = $@"
@@ -81,6 +48,13 @@ namespace DemoSitecoreCloud
             };
         }
 
+        /// <summary>
+        /// Retrieves paginated items from sitecore which is configured to return 50 items only
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <param name="first"></param>
+        /// <param name="afterCursor"></param>
+        /// <returns></returns>
         public static GraphQLRequest GetPaginatedGraphQLQuery(string itemId, int first, string afterCursor)
         {
             afterCursor = string.IsNullOrEmpty(afterCursor) ? "null" : $"\"{afterCursor}\"";
@@ -125,6 +99,63 @@ namespace DemoSitecoreCloud
             };
         }
 
+        /// <summary>
+        /// Create new item in sitecore
+        /// </summary>
+        /// <param name="itemName"></param>
+        /// <param name="nodes"></param>
+        /// <param name="templateId"></param>
+        /// <param name="parentId"></param>
+        /// <returns></returns>
+        public static GraphQLRequest CreateGraphQLQuery(string itemName, Dictionary<string, string> nodes, string templateId, string parentId)
+        {
+
+            var fieldsBuilder = new StringBuilder();
+            foreach (var node in nodes)
+            {
+                fieldsBuilder.Append($"{{name: \"{node.Key}\", value: \"{node.Value}\" }}");
+            }
+
+            var query = $@"
+                        mutation {{
+                            createItem(
+                                     input: {{ 
+                                             name: ""{itemName}""
+                                             templateId: ""{templateId}""
+                                             parent: ""{parentId}""
+                                             language: ""en""
+                                              fields[
+                                                 {fieldsBuilder.ToString().Trim()}
+                                            ]
+                                         }}
+                                     ){{
+                                        item {{
+                                            itemId
+                                            name
+                                            path    
+                                            fields(ownFields:true, excludeStandardfields: true){{
+                                                  nodes {{ 
+                                                           name 
+                                                           value
+                                                       }}
+                                                 }}
+                                            }}
+                                        }}
+                                 }}";
+            return new GraphQLRequest
+            {
+                Query = query,
+            };
+        }
+
+        /// <summary>
+        /// Update sitecore item values based on itemId and parentId
+        /// </summary>
+        /// <param name="itemId">Item id of node needs to be updated</param>
+        /// <param name="fields">fields contains name and new value of nodes to be updated</param>
+        /// <param name="templateId">template id of the node</param>
+        /// <param name="parentId">Parent id of the item needs to be updated</param>
+        /// <returns></returns>
         public static GraphQLRequest UpdateItemGraphQLQuery(string itemId, Dictionary<string, string> fields, string templateId, string parentId)
         {
             return new GraphQLRequest
@@ -160,5 +191,30 @@ namespace DemoSitecoreCloud
                 }
             };
         }
+
+        /// <summary>
+        /// Delete item from sitecore based on itemId
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public static GraphQLRequest DeleteItemGraphQLQuery(string itemId)
+        {
+            return new GraphQLRequest
+            {
+                Query = $@"
+                        mutation {{
+                            deleteItem(
+                                    input:{{ 
+                                             itemId: ""{itemId}""
+                                             permanently  : false
+                                        }}
+                                     ){{
+                                        successful      
+                                      }}
+                        }}"
+
+            };
+        }
+
     }
 }
